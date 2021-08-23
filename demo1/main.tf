ad62b442a5923354b10c57aca833f755f8a34df6
anishkalra08 <55384936+anishkalra08@users.noreply.github.com>
2021-08-23 14:43:25 +0530

    Create main.tf

---
+++ demo1/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  
+  required_version = ">=0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east2"
+}
+
+resource "aws_instance" "app_server" {
+  ami = "ami-0bff25b43a4479334"
+  instance_type = "t2.micro"
+  tags = {
+    name = "anish-1"
+  }
+}
